54459c32db5557f2cb027ecc535fd66c42d1866a
VClark24 <6clarv16@solent.ac.uk>
2026-05-26 11:49:53 +0100

    Create top_level.vhd

---
+++ AXI4 Templates/top_level.vhd
@@ -0,0 +1,126 @@
+--------------------------------------------------------------------------------------------------------------
+-- This is the top-level wrapper for the PRBS23 AXI Block. It was adapted from a generic AXI4-Lite Slave File
+--------------------------------------------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity  is --put entity name here
+  generic (
+    C_S00_AXI_DATA_WIDTH : integer := 32;
+    C_S00_AXI_ADDR_WIDTH : integer := 4
+  );
+  port (
+    -- AXI4-Lite Slave Interface
+    s00_axi_aclk    : in  std_logic;
+    s00_axi_aresetn : in  std_logic;
+
+    s00_axi_awaddr  : in  std_logic_vector(C_S00_AXI_ADDR_WIDTH-1 downto 0);
+    s00_axi_awprot  : in  std_logic_vector(2 downto 0);
+    s00_axi_awvalid : in  std_logic;
+    s00_axi_awready : out std_logic;
+
+    s00_axi_wdata   : in  std_logic_vector(C_S00_AXI_DATA_WIDTH-1 downto 0);
+    s00_axi_wstrb   : in  std_logic_vector((C_S00_AXI_DATA_WIDTH/8)-1 downto 0);
+    s00_axi_wvalid  : in  std_logic;
+    s00_axi_wready  : out std_logic;
+
+    s00_axi_bresp   : out std_logic_vector(1 downto 0);
+    s00_axi_bvalid  : out std_logic;
+    s00_axi_bready  : in  std_logic;
+
+    s00_axi_araddr  : in  std_logic_vector(C_S00_AXI_ADDR_WIDTH-1 downto 0);
+    s00_axi_arprot  : in  std_logic_vector(2 downto 0);
+    s00_axi_arvalid : in  std_logic;
+    s00_axi_arready : out std_logic;
+
+    s00_axi_rdata   : out std_logic_vector(C_S00_AXI_DATA_WIDTH-1 downto 0);
+    s00_axi_rresp   : out std_logic_vector(1 downto 0);
+    s00_axi_rvalid  : out std_logic;
+    s00_axi_rready  : in  std_logic;
+
+    -- User output to feed next IP
+    prbs_bit_out    : out std_logic;
+    prbs_step_i : in std_logic
+  );
+end ; --put entity name here
+
+architecture arch_imp of  is -- put entity name here
+
+  component S00_AXI is   -- slave wrapper name here
+    generic (
+      C_S_AXI_DATA_WIDTH : integer := 32;
+      C_S_AXI_ADDR_WIDTH : integer := 4
+    );
+    port (
+      -- User ports here
+      
+      -- AXI ports
+      S_AXI_ACLK     : in  std_logic;
+      S_AXI_ARESETN  : in  std_logic;
+
+      S_AXI_AWADDR   : in  std_logic_vector(C_S_AXI_ADDR_WIDTH-1 downto 0);
+      S_AXI_AWPROT   : in  std_logic_vector(2 downto 0);
+      S_AXI_AWVALID  : in  std_logic;
+      S_AXI_AWREADY  : out std_logic;
+
+      S_AXI_WDATA    : in  std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0);
+      S_AXI_WSTRB    : in  std_logic_vector((C_S_AXI_DATA_WIDTH/8)-1 downto 0);
+      S_AXI_WVALID   : in  std_logic;
+      S_AXI_WREADY   : out std_logic;
+
+      S_AXI_BRESP    : out std_logic_vector(1 downto 0);
+      S_AXI_BVALID   : out std_logic;
+      S_AXI_BREADY   : in  std_logic;
+
+      S_AXI_ARADDR   : in  std_logic_vector(C_S_AXI_ADDR_WIDTH-1 downto 0);
+      S_AXI_ARPROT   : in  std_logic_vector(2 downto 0);
+      S_AXI_ARVALID  : in  std_logic;
+      S_AXI_ARREADY  : out std_logic;
+
+      S_AXI_RDATA    : out std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0);
+      S_AXI_RRESP    : out std_logic_vector(1 downto 0);
+      S_AXI_RVALID   : out std_logic;
+      S_AXI_RREADY   : in  std_logic
+    );
+  end component;
+
+begin
+    S00_AXI_inst : S00_AXI -- slave wrapper
+    generic map (
+      C_S_AXI_DATA_WIDTH => C_S00_AXI_DATA_WIDTH,
+      C_S_AXI_ADDR_WIDTH => C_S00_AXI_ADDR_WIDTH
+    )
+    port map (
+      prbs_bit_o    => prbs_bit_out,
+      prbs_step_i   => prbs_step_i,
+
+      S_AXI_ACLK    => s00_axi_aclk,
+      S_AXI_ARESETN => s00_axi_aresetn,
+
+      S_AXI_AWADDR  => s00_axi_awaddr,
+      S_AXI_AWPROT  => s00_axi_awprot,
+      S_AXI_AWVALID => s00_axi_awvalid,
+      S_AXI_AWREADY => s00_axi_awready,
+
+      S_AXI_WDATA   => s00_axi_wdata,
+      S_AXI_WSTRB   => s00_axi_wstrb,
+      S_AXI_WVALID  => s00_axi_wvalid,
+      S_AXI_WREADY  => s00_axi_wready,
+
+      S_AXI_BRESP   => s00_axi_bresp,
+      S_AXI_BVALID  => s00_axi_bvalid,
+      S_AXI_BREADY  => s00_axi_bready,
+
+      S_AXI_ARADDR  => s00_axi_araddr,
+      S_AXI_ARPROT  => s00_axi_arprot,
+      S_AXI_ARVALID => s00_axi_arvalid,
+      S_AXI_ARREADY => s00_axi_arready,
+
+      S_AXI_RDATA   => s00_axi_rdata,
+      S_AXI_RRESP   => s00_axi_rresp,
+      S_AXI_RVALID  => s00_axi_rvalid,
+      S_AXI_RREADY  => s00_axi_rready
+    );
+
+end arch_imp;
